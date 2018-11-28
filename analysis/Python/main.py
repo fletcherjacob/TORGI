4d50fc7cd164748a2b4e1ebac0e66b6fb22fe4b1
Jacob Fletcher <jacobfletcher0293@floridapoly.edu>
2018-11-28 09:23:27 -0500

    Update main.py
    Added Annotation and Sampling capabilities

--- analysis/Python/main.py
+++ analysis/Python/main.py
@@ -2,12 +2,12 @@ import sqlite3
 import csv
 import os
 import pandas as pd
-
+import time
 
 def individual_output():
     con = sqlite3.connect(":memory:")
-    import_path = '/home/fletcher/dev/0_Torgi/data/'  ###Import Directory Path
-    descrip = 'EMI-'  ### FileId Category Identifier
+    import_path = ''  ###Import Directory Path
+    descrip = ''  ### FileId Category Identifier
     print(import_path)
     x = 0   ####FileId starting number
 
@@ -19,25 +19,57 @@ def individual_output():
             if filename.endswith(".gpkg"):
                 x = x + 1
                 sqlite_file = import_path + filename
-                conn = sqlite3.connect(sqlite_file)
+                conn = sqlite3.connect(sqlite_file)   #SQLITE connection
                 cur = conn.cursor()
-                data = cur.execute(
-                    "SELECT id, svid, constellation, cn0, agc, azimuth_deg,elevation_deg,FROM sat_Data;"  )  ### SQL selection statement
-                ##local_time = cur.execute("SELECT local_time FROM sat_Data")
-                export_csv = descrip +str(x) + '.csv'
+                data = cur.execute("SELECT svid, constellation, cn0, agc, local_time FROM sat_data;"  )  ### SQL selection statement
+                export_csv = descrip +str(x) + '.csv' #Sets new csv file to variable for writing
                 with open(export_csv, 'w') as csvf:
                     writer = csv.writer(csvf)
-                    writer.writerow(['id', 'svid', 'constellation', 'cn0', 'agc', 'azimuth_deg', 'elevation_deg', 'fileName', 'CONUS', 'fileId'])  ### Creats header row for CSV file
+                    writer.writerow( [ 'svid', 'constellation', 'cn0', 'agc', 'local_time','Attack'])  ### Creats header row for CSV file
                     writer.writerows(data)
                     conn.close()
 
             output_df = pd.read_csv(export_csv)
             ### Fills in Columns with Static data  to Columns added that were provided via the SQL statement
-            output_df['CONUS'] = '0'  ### Working to add automatic detection for future use
-            output_df['fileId'] = export_csv
-            output_df['fileName'] = filename
+           
+            """Determine Attack via given milisecond epoch time"""
+            output_df['Attack'] = (output_df['local_time'] >  1538602800000 & (output_df['local_time'] <  1538604360000))\ 
+                                 | (
+                        output_df['local_time'] > 1538609760000  & (output_df['local_time'] < 1538610600000))\
+                                  | (
+                        output_df['local_time'] > 1538617200000 & (output_df['local_time'] <  1538618520000))\
+                                  | (
+                        output_df['local_time'] > 1538618700000 & (output_df['local_time'] < 1538618940000))\
+                                  | (
+                        output_df['local_time'] > 1538619420000 & (output_df['local_time'] < 1538620980000))\
+                                  | (
+                        output_df['local_time'] > 1538621040000 & (output_df['local_time'] < 1538621940000))\
+                                  | (
+                        output_df['local_time'] > 1538622060000 & (output_df['local_time'] < 1538623740000))\
+                                  | (
+                        output_df['local_time'] > 1538624040000 & (output_df['local_time'] < 1538710440000))\
+                                  | (
+                        output_df['local_time'] > 153621040000 & (output_df['local_time'] < 1538621940000))\
+                                  | (
+                        output_df['local_time'] > 1538622060000 & (output_df['local_time'] < 1538623740000))\
+                                  | (
+                        output_df['local_time'] > 1538624040000 & (output_df['local_time'] < 1538710440000))\
+                                  | (
+                        output_df['local_time'] > 1538702220000 & (output_df['local_time'] < 1538704080000))\
+                                  | (
+                        output_df['local_time'] > 1538704080000 & (output_df['local_time'] < 1538705280000))\
+                                  | (
+                        output_df['local_time'] > 1538705460000 & (output_df['local_time'] < 1538706600000))\
+                                  | (
+                        output_df['local_time'] > 1538706660000 & (output_df['local_time'] < 1538707380000))\
+                                  | (
+                        output_df['local_time'] > 1538707500000 & (output_df['local_time'] < 1538708220000))\
+                                  | (
+                        output_df['local_time'] > 1538708340000 & (output_df['local_time'] < 1538698620000))
 
-            output_df.to_csv(export_csv)
+            update_data = output_df.fillna({'Attack': False})
+
+            update_data.to_csv(export_csv)
             print(export_csv)
 
         except sqlite3.Error:
@@ -50,10 +82,9 @@ def combine():
     with open(allOutput, 'w') as csvfile:
         fileWriter = csv.writer(csvfile)
         fileWriter.writerow(
-            ['id', 'svid', 'constellation', 'cn0', 'agc', 'azimuth_deg', 'elevation_deg', "fileName", "CONUS",
-             "fileId"])
+            [ 'svid', 'constellation', 'cn0', 'agc', 'local_time','Attack'])
 
-    csv_path = '/home/fletcher/dev/0_Torgi/2_Python/'
+    csv_path = '' #output path of Individual CSV
     original = pd.read_csv(csv_path + 'allOutput.csv')
     for f in os.listdir(csv_path):
 
@@ -72,14 +103,55 @@ def combine():
                 print(IOError)
 
     # combined_csv = pd.concat([original, original2], sort=True)
-    original.to_csv("combined_csv.csv", index=False)
+    original.to_csv("combined.csv", index=False)
     # print('Combined', original)
 
 
+def confirm():
+    test_file = 'combined.csv' #location of outputed csv from combine()
+    df = pd.read_csv(test_file)
+    print(df.head(n=5))
+    ### Fills in Columns with Static data  to Columns added that were provided via the SQL statement
+    is_attack = (df['Attack'] == True)
+    print(is_attack.value_counts()) #Shows Count of T & F , allows individual to know maximum size of sample possible
+
+def sample():
+    # making data frame from csv file
+    data1 = pd.read_csv('combined.csv')#location of outputed csv from combine()
+    data = data1.fillna({'Attack': False, 'CONUS': 0 })  #Fill in empty False 
+    print(data.head(5))
+    
+    attack_data = data[(data['Attack'] == True)]
+
+    print(data.head(5))
+    nonattack_data = data[(data['Attack'] == False)]
+    print(nonattack_data.head(5))
+
+    attack_sample = attack_data.sample(50000) # sample sizing
+    nonattack_sample = nonattack_data.sample(50000)
+    combine_sample = pd.concat([attack_sample, nonattack_sample])
+
+    ld = (len(data))
+    lad = (len(attack_sample))
+    lnad = (len(nonattack_sample))
+    print(ld)
+    print(lad)
+    print(lnad)
+    print(lad + lnad)
+
+
+
+    combine_sample.to_csv("sample_100k_50tf_v1.csv", index=False) #Naming Format, 100k = 100 Samples, 50tf = 50% tf, v1 = version
+    test = pd.read_csv("sample_100k_50tf_v1.csv")
+    if len(test) == 100000: #Test sample size
+        print("Success")
+    else:
+        print("Fail")
+
 def main():
     individual_output()
-    # all_output()
     combine()
-
+    confirm()
+    sample()
 
 main()
